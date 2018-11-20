ed29f51df91064889f5d87485cbd01b120e8988a
ming.tong@outlook.com <>
2018-11-20 19:07:18 +0800

    add video ui

--- CoreDriverBackend/wwwroot/jsbind/addVideo.js
+++ CoreDriverBackend/wwwroot/jsbind/addVideo.js
@@ -1,3 +1,15 @@
+var addVideo = new Vue({
+    el: "#addVideo",
+    data: {},
+    methods: {
+        submit: function (event) {
+            // `event` 是原生 DOM 事件
+            if (event) {
+                alert(event.target.tagName)
+            }
+        }
+    }
+});
 axios.post('api/Video', {
     Prefix: 'SW',
     Serial: '444',
